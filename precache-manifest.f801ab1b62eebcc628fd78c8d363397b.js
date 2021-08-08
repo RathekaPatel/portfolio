77d3b530e9be09068c714130333f7a02f23bae13
Yrgken <Yrgken@wilxite.com>
2021-08-08 11:29:32 +0100

    initial commit

---
+++ precache-manifest.f801ab1b62eebcc628fd78c8d363397b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8a026d23fe30680179d3f757738f07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea594b15836301b4c7d7",
+    "url": "./static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6c26351e21cb6581d57c",
+    "url": "./static/js/2.188da980.chunk.js"
+  },
+  {
+    "revision": "eff758343d82f754e4881ed2e608a50b",
+    "url": "./static/js/2.188da980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea594b15836301b4c7d7",
+    "url": "./static/js/main.ae0ed38e.chunk.js"
+  },
+  {
+    "revision": "1436ac51eb3085ff3d5c",
+    "url": "./static/js/runtime-main.c2538a3a.js"
+  }
+]);
